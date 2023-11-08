e409e9dd136d94cb27d01ab968c58e240f3b5536
erte <erik.teppan@nordicsemi.no>
2023-11-08 15:40:14 +0100

    Added more plots and fixed integral of field through measuring loop

--- CurrentLIdeal.py
+++ CurrentLIdeal.py
@@ -6,15 +6,12 @@ xAxis = np.linspace(0, 40e-9, num=400)
 # Assuming perfect square wave
 # V = 100*[0]+300*[5]+100*[0]
 # Using measured square wave
-dfMeas = pd.read_csv(f"150_66.0,154.9,7.5_0.csv", usecols=['Channel A', 'Channel C'])
-V = dfMeas['Channel A']
-Emeas = dfMeas['Channel C']
 # Assuming ideal inductor
 n = 5
 l = 6e-3
 radius = 2e-3
 mu0  = 1.257e-6  #m kg s**-2 A**-2
-mur = 1500
+mur = 30
 #Magnetic permeability of metals is usually between 1500 to 3000
 mu = mu0 * mur
 ZL = n**2*radius**2*np.pi*mu/l
@@ -22,24 +19,9 @@ ZL = n**2*radius**2*np.pi*mu/l
 print(f'{ZL*1000} mH')
 # ZL = 400e-9 # 0.4 uH
 R = 1.8 # Ohm
+Rloss = 2
+C = 5e-12 # 5 pF
 dt = 0.4e-9 #400 ps
-
-# Voltage over inductor that opposes change in current
-VL = [0]
-# Current flowing through inductor
-I = []
-
-# Algorithm depends on last value, so have to do index 0 "manually"
-didt = VL[-1]/ZL
-I.append(didt*dt)
-# VR = I[0]*R
-# VL = V[0]-VR
-
-for i, v in enumerate(V[1:]):
-  VR = I[-1]*R
-  VL.append(v-VR)
-  didt = VL[-1]/ZL
-  I.append(I[-1]+didt*dt)
 
 # Loop position/size
 rLoop = 3e-2/2 #d=3cm
@@ -53,43 +35,106 @@ endr = 4e-2
 numr = 100
 
 numTheta = 100
+dtTheta = 2*np.pi/numTheta
 
 zArr = np.linspace(startZ,  endZ, num=numZ)
 rArr = np.linspace(startr,  endr, num=numr)
 theta1Arr = np.linspace(0, 2*np.pi, num=numTheta)
+dr = (endr-startr)/numr
 
+# 4 for 4mm
 df4 = pd.read_csv(f"magneticFieldR0.002_{startZ}-{endZ}-{numZ}_{startr}-{endr}-{numr}_{numTheta}.csv", usecols=['B'])
 
 B4 = np.array(df4.B)
 B4 = B4.reshape(numZ, numr, numTheta)
 
-
 sumOfField4 = 0
 z = 6
+# TODO: Account for dr and d0. Right now we only use them as point sources, but we need the integral, which contains dr
+# from scipy.integrate import dblquad
+# sumOfField4 = dblquad(lambda r, theta: B4[z], )
 for j, r in enumerate(rArr):
     if r<=rLoop:
         for k, theta in enumerate(theta1Arr):
-            sumOfField4 += B4[z][j][k]
-sumOfField4 = sumOfField4*mur
-print(sumOfField4)
+            sumOfField4 += B4[z][j][k]*dr*dtTheta*2*(r+dr)
+sumOfField4 = sumOfField4
 
 fig = plt.figure()
-ax = fig.add_subplot(121)
-# ax.plot(I, 'orange', label='Current through L')
-ax2 = ax.twinx()
-ax.plot(V, label='Input Voltage')
-# ax2.plot(I, 'orange', label='Inductor Current')
-ax2.plot(np.gradient(I)-np.average(np.gradient(I)[0:400]), 'orange', label='Emf')
-# ax2.plot(sumOfField4*np.gradient(I), 'orange', label='Calculated Emf')
-ax.legend(loc='upper left')
-ax2.legend(loc='upper right')
-ax = fig.add_subplot(122)
-# ax.plot(I, 'orange', label='Current through L')
-ax2 = ax.twinx()
-ax.plot(Emeas, label='Emf measured')
-# ax2.plot(I, 'orange', label='Inductor Current')
-ax2.plot(sumOfField4*np.gradient(I), 'orange', label='Emf')
-# ax2.plot(sumOfField4*np.gradient(I), 'orange', label='Calculated Emf')
-ax.legend(loc='upper left')
-ax2.legend(loc='upper right')
+ax = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+
+Iarr = []
+EmfArr = []
+relDiffPeak = []
+for width in range(100, 550, 50):
+  dfMeas = pd.read_csv(f'C:\\ProsjektOppgave\\emfi-studies\\_results_ZCentral\\WidthDependence\\raw_dumps\\150V\\width_{width}offset_0\\150_66.0,154.9,20_0.csv')
+  Vtot = np.array(dfMeas['Channel A'])
+  Etot = np.array(dfMeas['Channel C'])
+  for i in range(1,10,1):
+    dfMeas = pd.read_csv(f'C:\\ProsjektOppgave\\emfi-studies\\_results_ZCentral\\WidthDependence\\raw_dumps\\150V\\width_{width}offset_0\\150_66.0,154.9,20_{i}.csv')
+    Vtot += np.array(dfMeas['Channel A'])
+    Etot += np.array(dfMeas['Channel C'])
+
+  V = Vtot/10
+  V = V - np.average(V[0:60])
+  Emeas = Etot/10
+  VR = [] 
+  # Voltage over inductor that opposes change in current
+  VL = [0]
+  VR = [0]
+  # Current flowing through inductor
+  I = []
+
+  # Algorithm depends on last value, so have to do index 0 "manually"
+  didt = VL[-1]/ZL
+  Ic = C*V[0]/dt
+  I.append(didt*dt)
+  # VR = I[0]*R
+  # VL = V[0]-VR
+  VR.append((I[-1]+Ic)*Rloss+R)
+  VL.append(VR[-1])
+
+  for i, v in enumerate(V[1:]):
+    didt = VL[-1]/ZL
+    Ic = C*(v-V[i])/dt
+    I.append(I[-1]+didt*dt)
+    VR.append((I[-1]+Ic)*Rloss+R)
+    VL.append(v-VR[-1])
+  Iarr.append(I)
+  EmfArr.append(sumOfField4*np.gradient(I))
+  relDiffPeak.append(max(I)/max(EmfArr[-1]))
+
+
+  # ax.plot(V, label=f'Input Voltage width: {width}')
+  # # ax2.plot(I, 'orange', label='Inductor Current')
+  # ax2.plot(np.gradient(I)-np.average(np.gradient(I)[0:400]), label=f'Emf width: {width}')
+  # # ax2.plot(sumOfField4*np.gradient(I), 'orange', label='Calculated Emf')
+  # ax.legend(loc='upper left')
+  # ax2.legend(loc='upper right')
+  # ax = fig.add_subplot(122)
+  # ax.plot(I, 'orange', label='Current through L')
+  # ax.plot(Emeas, 'orange', label=f'Emf measured, width: {width}')
+  # ax2.plot(I, 'orange', label='Inductor Current')
+  ax.plot(I, label=f'Current theoretical, width: {width}')
+  # ax2.plot(sumOfField4*np.gradient(I), 'orange', label='Calculated Emf')
+  ax.legend(loc='right')
+  ax2.plot(VL, label=f'Voltage inductor theoretical, width: {width}')
+  # ax2.plot(sumOfField4*np.gradient(I), 'orange', label='Calculated Emf')
+  ax2.legend(loc='upper right')
+  ax3.plot(V, label=f'Voltage, width: {width}')
+  ax3.legend(loc='right')
+  # ax2.legend(loc='upper right')
+
+
+ax2 = fig.add_subplot(224)
+
+for width in range(100, 500, 50):
+  dfMeas = pd.read_csv(f'C:\\ProsjektOppgave\\emfi-studies\\_results_ZCentral\\WidthDependence\\raw_dumps\\150V\\width_{width}offset_0\\150_66.0,154.9,20_0.csv')
+  V = dfMeas['Channel A']
+  V = V - np.average(V[0:60])
+  Emeas = dfMeas['Channel C']
+  ax2.plot(Emeas, label=f'Emf measured width: {width}')
+ax.legend()
+ax2.legend()
 plt.show()
